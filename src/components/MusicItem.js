f10ca58d75b096c7515e557674c606a92dc4be16
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-01-12 23:46:57 +0000

    Tidied up some code

--- src/components/MusicItem.js
+++ src/components/MusicItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const MusicItem = ({song, position}) => {
     return(
@@ -6,7 +6,7 @@ const MusicItem = ({song, position}) => {
         <h3>#{position}: {song['im:name']['label']}</h3>
         <h5>Artist: {song['im:artist']['label']}</h5>
         </>
-    )
+    );
 };
 
 export default MusicItem;
--- src/components/MusicList.js
+++ src/components/MusicList.js
@@ -1,9 +1,8 @@
-import React from "react";
-import MusicItem from "./MusicItem";
+import React from 'react';
+import MusicItem from './MusicItem';
 
 const MusicList = ({music, title}) => {
     const songs = music.map((song, index) => {
-        // song.
         return <MusicItem song={song} key={index} position={index + 1}/>
     });
   
