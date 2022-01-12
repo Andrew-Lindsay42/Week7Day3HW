15129d06c412d5488e30930eba085074c0bb693b
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-01-12 23:35:22 +0000

    Added a genre selector

--- src/containers/MusicBox.js
+++ src/containers/MusicBox.js
@@ -5,22 +5,38 @@ const MusicBox = () => {
     const [music, setMusic] = useState([]);
     const [title, setTitle] = useState("");
     let result = null;
+    const url_prefix = 'https://itunes.apple.com/gb/rss/topsongs/limit=20'
+    let url_suffix = '/json'
 
     useEffect(() => {
       getMusic();
     }, [])
 
     const getMusic = function(){
-        fetch('https://itunes.apple.com/gb/rss/topsongs/limit=20/json')
+        fetch(url_prefix + url_suffix)
         .then(res => res.json())
         .then(res => result = res)
         .then( () => setTitle(result['feed']['title']['label']))
         .then( () => setMusic(result['feed']['entry']))
+    }
 
+    const handleChange = function(event) {
+        url_suffix = event.target.value;
+        getMusic();
     }
 
     return (
         <div className="main-container">
+            <select onChange={handleChange}>
+                <option value="/json">Choose a genre</option>
+                <option value="/json">All</option>
+                <option value="/genre=4/json">Children's Music</option>
+                <option value="/genre=14/json">Pop</option>
+                <option value="/genre=21/json">Rock</option>
+                <option value="/genre=17/json">Dance</option>
+                <option value="/genre=6/json">Country</option>
+
+            </select>
             <MusicList music = {music} title = {title}/>
         </div>
     )
